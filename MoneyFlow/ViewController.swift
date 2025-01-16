5f02b1dcde65d9a0de6051f7e052cd87d602cc44
Hypocrite1023 <rex901023wot@gmail.com>
2025-01-16 16:30:31 +0800

    Initial Commit

---
+++ MoneyFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyFlow
+//
+//  Created by YI-CHUN CHIU on 2025/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
